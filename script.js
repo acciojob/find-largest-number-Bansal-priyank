91987646785921a219715ac2a74c2680204c2bcb
Priyank Bansal <165632145+Bansal-priyank@users.noreply.github.com>
2025-08-12 23:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function findLargest(a, b, c) {
+	let largest;
+
+	if (a >= b && b >= c) {
+		largest = a;
+	} else if (b >= a && b >= c){
+		largest = b;
+	}
+	else {
+		largest = c;
+	}
+	return largest;
   //your code here
 }
+console.log(findLargest(100, 200, 50));   // 200
+console.log(findLargest(-100, -200, -50)); // -50
+console.log(findLargest(-100, 200, 50));   // 200
+console.log(findLargest(0, 0, 0));   
 
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
